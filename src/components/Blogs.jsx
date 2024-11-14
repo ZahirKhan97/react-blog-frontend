5311fd39a7f981f479888b4023526d033e312255
MZK <139211996+ZahirKhan97@users.noreply.github.com>
2024-11-14 09:40:03 +0500

    Searching Blogs Integrated

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -3,17 +3,38 @@ import BlogCard from './BlogCard'
 
 const Blogs = () => {
   const [blogs, setBlogs] = useState();
+  const [keyword, setKeyword] = useState('');
   const fetchBlogs = async () => {
     const res = await fetch("http://localhost:8000/api/blogs");
     const result = await res.json();
     setBlogs(result.data);
   }
 
+  const searchBlogs = async (e) => {
+    e.preventDefault();
+    const res = await fetch("http://localhost:8000/api/blogs?keyword="+keyword);
+    const result = await res.json();
+    setBlogs(result.data);
+  }
+
+  const resetSearch = () => {
+    fetchBlogs();
+    setKeyword('');
+  }
   useEffect(() => {
     fetchBlogs();
   },[])
   return (
     <div className="container">
+        <div className="d-flex justify-content-center pt-5 pb-4">
+          <form onSubmit={(e) => searchBlogs(e)}>
+            <div className='d-flex'>
+              <input type="text" value={keyword} onChange={(e) => setKeyword(e.target.value)} className="form-control" placeholder='Search Blog'/>
+              <button className='btn btn-dark ms-2'>Search</button>
+              <button onClick={() => resetSearch()} className='btn btn-success ms-2'>Reset</button>
+            </div>
+          </form>
+        </div>
         <div className="d-flex justify-content-between pt-5 pb-4">
           <h4>Blogs</h4>
           <a href="/create" className='btn btn-dark'>Create</a>
